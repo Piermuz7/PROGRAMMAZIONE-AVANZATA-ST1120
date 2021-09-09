35b39db242fa5a4fb0d0419e0b98d729b64ca30f
Piermuz <81294932+piermichele-rosati@users.noreply.github.com>
2021-09-09 16:21:15 +0200

    Add files via upload

---
+++ PatternMatching/pm.fs
@@ -0,0 +1,26 @@
+﻿let rec conta l =   // conta: l: float list -> float
+    match l with
+    | [] -> 0.0
+    | v::tail -> v + (conta tail)
+
+
+let isVocal v =
+    if(v='a' || v='e' || v='i' || v='o' || v='u') then true
+    else false
+
+let rec contaVocali s vocali= 
+    match s with
+    | [] -> 0
+    | v::tail -> if(isVocal v) 
+                 then contaVocali tail vocali+1
+                 else
+                      contaVocali tail vocali
+
+open System
+
+[<EntryPoint>]
+let main argv =
+    printfn "Somma della lista = %f" (conta [1.0 .. 10.0])
+    printfn "Numero vocali di aiuola = %d" (contaVocali ['a';'i';'u';'o';'l';'a'] 0)
+    printfn "Numero vocali di Piermuz = %d" (contaVocali ['P';'i';'e';'r';'m';'u';'z'] 0)
+    0
