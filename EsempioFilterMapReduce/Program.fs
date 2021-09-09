c59fb7b52c85e71a93224db4779ed569c9ff6f91
Piermichele Rosati <piermichele.rosati@gmail.com>
2021-09-09 15:42:55 +0200

    First commit

---
+++ EsempioFilterMapReduce/Program.fs
@@ -0,0 +1,26 @@
+﻿// Funzione che presa una lista, restituisce la somma dei quadrati di tutti i numeri pari.
+// Filter: numeri pari
+// Map: x -> x^2
+// Reduce: Somma di tutti gli x^2
+
+// Predicate numeri pari
+let evenNumber x =  // x: int -> bool
+    x%2=0;
+
+// Map quadrato dei numeri
+let powNumber x = // x: int -> int
+    x*x;
+
+// Reduce
+let sum x y =   // x: int -> y: int -> int
+    x+y;
+
+let sumPowNumbers list =    // list: int list -> int
+    List.reduce (sum) (List.map (powNumber) (List.filter (evenNumber) list ));
+ 
+open System
+
+[<EntryPoint>]
+let main argv =
+    printfn "Somma dei quadrati dei primi 10 numeri pari = %d" (sumPowNumbers [1 .. 10]);
+    0 // return an integer exit code
